3a066e0eca3262b9fc9c4ecb4fef7a641f55ed0d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2018-11-15 11:47:40 -0500

    initial

---
+++ server/src/modules/app.ts
@@ -0,0 +1,29 @@
+import { GraphQLModule } from '@graphql-modules/core';
+import { AccountsModule } from '@accounts/graphql-api';
+import AccountsServer from '@accounts/server';
+import { Db } from 'mongodb';
+import { IncomingMessage } from 'http';
+import { PostsModule } from './post';
+import { DbModule } from './db';
+
+export interface AppModuleConfig {
+    accountsServer: AccountsServer;
+    db: Db;
+}
+
+export interface AppModuleRequest {
+    req: IncomingMessage;
+}
+
+export const AppModule = new GraphQLModule<AppModuleConfig>({
+    name: 'app',
+    imports: ({ config }) => [
+        AccountsModule.forRoot({
+            accountsServer: config.accountsServer
+        }) as any,
+        DbModule.forRoot({
+            db: config.db
+        }),
+        PostsModule,
+    ],
+});
